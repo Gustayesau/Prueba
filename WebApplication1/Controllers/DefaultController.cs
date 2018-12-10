522490ecd591e5a5659366f4450564f826d68393
unknown <SPUNIVO20@SPUNIVO20.sv.cds>
2018-12-10 10:37:07 -0600

    lol

---
+++ WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+   
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            //Estudiante c = new Estudiante();           
+            Estudiante c = new Estudiante();
+            //    List<string> materias = new List<string>()
+            //    {
+            //        "matematica","algoritmia","mvc basico",
+            //        "historia del internet","use tools","linq"
+            //};
+            //    var query = from n in materias
+            //                where n.StartsWith("m")
+            //                select n;
+            if (Request.HttpMethod == "POST")
+            {
+                string elejir = Request["g"];
+                if (elejir == "N")
+                {
+                string buscar = Request["nom"];
+                var query = from a in c.Lista() where a.Nombre.Contains(buscar) select a;
+                return View(query.ToList());
+                }else if (elejir=="M")
+                {
+                    string buscar = Request["nom"];
+                    var query = from a in c.Lista() where a.Materia.Contains(buscar) select a;
+                    return View(query.ToList());
+                }
+                else if (elejir == "P")
+                {
+                    string buscar = Request["nom"];
+                    int bus =Convert.ToInt32(buscar);
+                    
+                    var query = (from a in c.Lista() where a.Promedio==bus select a);
+                    return View(query.ToList());
+                }
+
+            }
+           
+            return View(c.Lista().ToList());
+        }
+        
+
+    }
+}
---
+++ WebApplication1/Models/Estudiante.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public int Carnet { get; set; }
+        public string Nombre { get; set; }
+        public string Materia { get; set; }
+        public int Promedio { get; set; }
+        public DateTime fecha { get; set; }
+
+        public List<Estudiante> Lista()
+        {
+            //Estudiante nuevo = new Estudiante()
+            //{
+              
+            //    Carnet = 1,
+            //    Nombre = "Gustavo",
+            //    Materia = "Matematica",
+            //    Promedio = 10,
+            //    fecha = DateTime.Now
+            //};
+            //Estudiante nuevo2 = new Estudiante()
+            //{
+               
+            //    Carnet = 1,
+            //    Nombre = "Gustavo",
+            //    Materia = "Matematica",
+            //    Promedio = 10,
+            //    fecha = DateTime.Now
+            //};
+            //Estudiante nuevo3 = new Estudiante()
+            //{
+             
+            //    Carnet = 1,
+            //    Nombre = "Gustavo",
+            //    Materia = "Matematica",
+            //    Promedio = 10,
+            //    fecha = DateTime.Now
+            //};
+            //Estudiante nuevo4 = new Estudiante()
+            //{
+                
+            //    Carnet = 1,
+            //    Nombre = "Gustavo",
+            //    Materia = "Matematica",
+            //    Promedio = 10,
+            //    fecha = DateTime.Now
+            //};
+            var estudiante = new Estudiante()
+            {            
+                Carnet = 1,
+                Nombre = "Gustavo",
+                Materia = "Matematica",
+                Promedio = 10,
+                fecha = DateTime.Now
+            };
+            var estudiante2 = new Estudiante()
+            {
+                Carnet = 2,
+                Nombre = "Juan",
+                Materia = "Lenguaje",
+                Promedio = 9,
+                fecha = DateTime.Now
+            };
+            var estudiante3 = new Estudiante()
+            {
+                Carnet = 3,
+                Nombre = "Julio",
+                Materia = "Ciencia",
+                Promedio = 8,
+                fecha = DateTime.Now
+            };
+
+            return new List<Estudiante>() {estudiante,estudiante2,estudiante3};
+            
+        }
+       
+    }
+
+}
